eec608bda0dc409b1ea334436f76dc705efb1de5
Artem Kharshan <timkharshan@hotmail.com>
2017-10-05 17:15:00 -0700

    fixed all

--- board.rb
+++ board.rb
@@ -3,21 +3,21 @@ require_relative "tile"
 class Board
   attr_reader :grid
 
-  def self.empty_grid
+  def empty_grid
     @grid = Array.new(9) do
       Array.new(9) { Tile.new(0) }
     end
     grid
   end
 
-  def self.from_file(filename)
-    rows = File.readlines("filename").map(:chomp)
+  def from_file(filename)
+    rows = File.readlines(filename).map(&:chomp)
     tiles = rows.map do |row|
-      nums = row.split("").map { |char| parseInt(char) }
-      nums.map { |num| Tle.new(num) }
+      nums = row.split("").map { |char| Integer(char) }
+      nums.map { |num| Tile.new(num) }
     end
 
-    self.new(tiles)
+    Board.new(tiles)
   end
 
   def initialize(grid = self.empty_grid)
@@ -25,14 +25,14 @@ class Board
   end
 
   def [](pos)
-    pos = x,y
+    x,y = pos
     grid[x][y]
   end
 
   def []=(pos, value)
     x, y = pos
     tile = grid[x][y]
-    tile.value = new_value
+    tile.value = value
   end
 
   def columns
@@ -40,7 +40,7 @@ class Board
   end
 
   def render
-    puts "(0..8).to_a.join(" ")"
+    puts "  #{(0..8).to_a.join(" ")}"
     grid.each_with_index do |row, i|
       puts "#{i} #{row.join(" ")}"
     end
@@ -51,7 +51,10 @@ class Board
     grid.size
   end
 
-  alias_method :rows, :size
+  def rows
+    @grid.map {|row| row}
+  end
+  # alias_method :rows, :size
 
   def solved?
     rows.all? { |row| solved_set?(row) } &&
